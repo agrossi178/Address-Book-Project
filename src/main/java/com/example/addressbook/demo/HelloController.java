5680f55d08d25953718f0f18d618a36756bea3bc
Alex <alexander.grossi@connect.qut.edu.au>
2026-04-23 18:48:19 +1000

    Added functioning cancel feature

--- src/main/java/com/example/addressbook/demo/HelloController.java
+++ src/main/java/com/example/addressbook/demo/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.addressbook.demo;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -7,25 +9,25 @@ import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
-    @FXML
     private CheckBox checkBox;
 
     @FXML
     private Button next;
 
-    private boolean accepted;
-
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Button cancel;
+
+    private boolean accepted;
 
     @FXML
     protected void onAgreeCheckBoxClick() {
         accepted = checkBox.isSelected();
         next.setDisable(!accepted);
+    }
+
+    @FXML
+    protected void onCancelClick(ActionEvent event) {
+        Platform.exit();
     }
 
     public void initialize() {
