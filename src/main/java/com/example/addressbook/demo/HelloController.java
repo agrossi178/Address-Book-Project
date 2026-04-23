d320a1ab7d4b92310db7351f0ec615afb167e5f0
Alex <alexander.grossi@connect.qut.edu.au>
2026-04-23 18:57:51 +1000

    Added functioning next feature

--- src/main/java/com/example/addressbook/demo/HelloController.java
+++ src/main/java/com/example/addressbook/demo/HelloController.java
@@ -3,9 +3,14 @@ package com.example.addressbook.demo;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
@@ -28,6 +33,14 @@ public class HelloController {
     @FXML
     protected void onCancelClick(ActionEvent event) {
         Platform.exit();
+    }
+
+    @FXML
+    protected void onNextButtonClick() throws IOException {
+        Stage stage = (Stage) next.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 700, 500);
+        stage.setScene(scene);
     }
 
     public void initialize() {
