e7ebab0a720c1e9e8e9613831b2d432193ac8672
Alex <alexander.grossi@connect.qut.edu.au>
2026-04-23 20:48:34 +1000

    Added contact methods and mock contact data

--- src/main/java/com/example/addressbook/demo/Contact.java
+++ src/main/java/com/example/addressbook/demo/Contact.java
@@ -8,7 +8,7 @@ public class Contact {
     private String email;
     private String phone;
 
-    public Contact(int id, String firstName, String lastName, String email, String phone) {
+    public Contact(String firstName, String lastName, String email, String phone) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
